8870c54c59d943494254540c9f353c2d3073422f
ntalex <alex@thuering.biz>
2015-10-11 13:09:59 +0200

    updated

---
+++ build_release.sh
@@ -0,0 +1,10 @@
+MODULE=MP4Tools-3.2
+CODE_DIR=mp4joiner-git
+
+rm -rf $CODE_DIR
+git clone git://git.code.sf.net/p/mp4joiner/git $CODE_DIR || exit 1
+(cd $CODE_DIR && chmod +x autogen.sh && ./autogen.sh && rm -rf autom4te.cache .git)
+mv "$CODE_DIR" "$MODULE"
+
+tar cjvf $MODULE.tar.bz2 $MODULE
+rm -rf $MODULE
